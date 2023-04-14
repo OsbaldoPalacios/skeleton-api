6ed46a14ff1a0beadb82ffe518bdf191c5a9089c
OsbaldoPalacios <osbaldo.palacios@uaem.edu.mx>
2023-04-14 09:06:25 -0600

    add auth[controllers,services,router]

--- src/users/users.controllers.js
+++ src/users/users.controllers.js
@@ -2,8 +2,31 @@ const Users = require('../models/users.models');
 const uuid = require('uuid');
 const { hashPassword } = require('../utils/crypto');
 
-const createNewUser = async(userObj) => {
-    const newUser = {
+const findAllUsers = async () => {
+    const users = await Users.findAll()
+    return users
+}
+
+const findUserById = async (id) => {
+    const data = await Users.findOne({
+        where: {
+            id: id
+        }
+    })
+    return data
+}
+
+const findUserByEmail = async (email) => {
+    const data = await Users.findOne({
+        where: {
+            email: email
+        }
+    })
+    return data
+}
+
+const createUser = async(userObj) => {
+    const newUser = await Users.create({
         id: uuid.v4(),
         firstName: userObj.firstName,
         lastName: userObj.lastName,
@@ -11,7 +34,36 @@ const createNewUser = async(userObj) => {
         password: hashPassword(userObj.password),
         birthday: userObj.birthday,
         phone: userObj.phone
-    }
-    const data = await Users.create(newUser)
-    return data
+    })
+    return newUser
+}
+
+const updateUser = async(id, userObj) => {
+    const selectedUser = await Users.findOne({
+
+        where: {
+            id: id
+        }
+    })
+    if(!selectedUser) return null
+    const modifiedUser = await selectedUser.update(userObj)
+    return modifiedUser
+}
+
+const deleteUser = async(id) => {
+    const user = await Users.destroy({
+        where: {
+            id: id
+        }
+    })
+    return user
+}
+
+module.exports = {
+    findAllUsers,
+    findUserById,
+    findUserByEmail,
+    createUser,
+    updateUser,
+    deleteUser
 }
